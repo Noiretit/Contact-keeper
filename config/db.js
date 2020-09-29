abbd8188223709bfeaf752be63c7be3660ce5968
Noiret <sergio.ko@icloud.com>
2020-09-29 11:20:30 +0200

    User model created, added middleware for json, added validation to users route

--- config/db.js
+++ config/db.js
@@ -6,7 +6,8 @@ const connectDB = () => {
     mongoose.connect(db, {
         useNewUrlParser: true,
         useCreateIndex: true,
-        useFindAndModify: false
+        useFindAndModify: false,
+        useUnifiedTopology: true
     })
     .then(() => console.log('MongoDB Connected'))
     .catch((e) => {
