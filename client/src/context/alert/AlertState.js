22b6ef9956dce8409280c9c7d3ecb44206468b1a
Noiret <sergio.ko@icloud.com>
2020-10-01 16:37:55 +0200

    Created Alert Context+Reducer+State: Reducer has case SET_ALERT which returns a copy of the state and the action payload and REMOVE_ALErt which removes the alert (duh). State: has method setAlert which dispatches a message with a type and then a setTimeout to remove the alert, state and method are passed to the AlertContext Provider. || Created Alerts.js component which uses Alert Context and returns a div with an icon and a classname depending on the alert

---
+++ client/src/context/alert/AlertState.js
@@ -0,0 +1,42 @@
+import React, { useReducer } from 'react';
+import {v4 as uuid} from 'uuid';
+
+import AlertContext from './AlertContext';
+import AlertReducer from './AlertReducer';
+import {
+    SET_ALERT,
+    REMOVE_ALERT
+} from '../types';
+
+const AlertState = props => {
+    const initialState = [];
+
+    
+    const [state, dispatch] = useReducer(AlertReducer, initialState);
+
+    //All our actions down below
+
+    //Set alert
+    const setAlert = (msg, type, timeout = 5000) => {
+        const id = uuid;
+        dispatch({
+            type: SET_ALERT,
+            payload: {msg, type, id}
+        });
+        setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout)
+    }
+
+    return (
+        <AlertContext.Provider value={{
+            alerts: state,
+            setAlert
+        }}>
+            { props.children }
+        </AlertContext.Provider>
+    )
+};
+
+//In order to use this Provider we have to wrap our App.js
+//This file will allow any component to access this state and actions, avoiding prop.drilling
+
+export default AlertState;
